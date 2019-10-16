a82bbc59bc6ceb0c6f91fdcd37f18a4ed364703a
Oscar Cederberg <os7138ce-s@stundent.lu.se>
2019-10-16 16:42:54 +0200

    Add project files.

---
+++ nekobasu-rpg-roguelite/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nekobasu_rpg_roguelite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
